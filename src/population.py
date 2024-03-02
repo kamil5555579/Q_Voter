b363bef73af56bcea0294be56e12e743c3f46034
TymonOrl <01169766@pw.edu.pl>
2024-03-02 15:34:50 +0100

    Working workframe for simulation
    Added Classes Voter and Voters_network with ability to check for consensus in lobby and random noise generation. Also now lobby takes into concideration connection in network witch was implemented with line 53 in population.py

--- src/population.py
+++ src/population.py
@@ -2,14 +2,15 @@ import numpy as np
 
 
 class Voters:
-    def __init__(self, size):
-        self.voters = np.zeros(size, dtype=bool)
+    def __init__(self, size: int, initial_c: float):
+        self.voters = np.ones(size)
         rng = np.random.default_rng()
         random_indexes = rng.choice(
                     size, 
-                    size = int(size/2), 
+                    size = int(size * initial_c), 
                     replace = False) # Replace flag makes all numbers unique
-        self.voters[random_indexes] = 1
+        self.voters[random_indexes] = -1
+        self.update_c()
 
     def get_size(self):
         return len(self.voters)
@@ -17,13 +18,29 @@ class Voters:
     def get_values(self, indexes):
         return self.voters[indexes]
 
+    def update_c(self):
+        self.c = self.voters[self.voters == 1].sum() / self.get_size()
+
+    def get_c(self):
+        self.update_c()
+        return self.c
+
+    def try_to_change_opinion_of(self, 
+        index: int, 
+        voters_net):
+        if (voters_net.is_opinion_changed(index, self.voters[index])):
+            self.voters[index] *= (-1)
+            #print(str(index) + " was changed")
+
     def __str__(self):
-        return "Voters: % s\n" % (1*self.voters)
+        return "Voters: % s, c: % s" % (self.voters, self.get_c())
 
 
 class Voters_net:
-    def __init__(self, voters, connection_type = "all connected"):
+    def __init__(self, voters, q: int, p: float, connection_type: str = "all connected"):
         self.voters = voters
+        self.q = q
+        self.p = p
         size = voters.get_size()
 
         if connection_type == "all connected":
@@ -31,31 +48,46 @@ class Voters_net:
             for i in range(size):
                 self.network[i,i] = None
 
-    def lobby_consensus(self, q):
+    def lobby_consensus(self, voters_index: int):
         rng = np.random.default_rng()
+        connections = np.arange(self.voters.get_size())[self.network[voters_index]]
         random_indexes = rng.choice(
-                    self.voters.get_size(), 
-                    size = q, 
+                    connections, 
+                    size = self.q, 
                     replace = False)
         lobby = self.voters.get_values(random_indexes)
-        if lobby.all():
-            print("In favor: " + str(lobby))
-            return True
-        elif (~lobby).all():
-            print("In against: " + str(lobby))
-            return False
+        if np.all(lobby == 1):
+            #print("In favor: " + str(lobby))
+            return 1
+        elif np.all(lobby == -1):
+            #print("In against: " + str(lobby))
+            return -1
         else:
             return None
 
+    def is_opinion_changed(self, voters_index: int ,voters_opinion: int):
+        #print("voters_opinion: " + str(voters_opinion))
+        lobby_consensus = self.lobby_consensus(voters_index)
+        result = False
+        if np.random.random_sample() < self.p:
+            if np.random.random_sample() < 0.5: # p/2 propability of fliping
+                #print("Noise")
+                result = True
+        else: # 1-p propability of looking for consenus
+            match voters_opinion:
+                case -1:
+                    if lobby_consensus == 1:
+                        if np.random.random_sample() < (1 - self.p):
+                            result = True
+                case 1:
+                    if lobby_consensus == -1:
+                        if np.random.random_sample() < (1 - self.p):
+                            result = True
+
+        return result
+            
+
     def __str__(self):
         output = str(self.voters)
-        output += "Network:\n" + str(1*self.network)
+        output += "\nNetwork:\n" + str(1*self.network)
         return(output)
-
-# Main tmp
-
-voters1 = Voters(10)
-net1 = Voters_net(voters1)
-for _ in range(10):
-    net1.lobby_consensus(3)
-print(net1)
--- src/voter.py
+++ src/voter.py
@@ -1,24 +1,42 @@
 import numpy as np
+from population import Voters, Voters_net 
+from progressbar import progressbar
+
 
 class VoterModel():
-    def __init__(self, N: int, q: int, p: float, M: int) -> None:
+    def __init__(self, N: int, q: int, p: float, M: int, c: float) -> None:
         """
         Initializes a voter model object
         :param N: Number of voters
         :param q: Size of lobby
         :param p: Probability of independent voting
         :param M: Number of Monte Carlo steps
+        :param c: Initial concentration of 1s
         """
         self.N = N
         self.q = q
         self.p = p
         self.M = M
 
+        self.voters = Voters(N, c)
+        self.network = Voters_net(self.voters, q, p)
+        print(self.network)
+
     def run_simulation(self) -> None:
         """
         Runs the voter model simulation
         """
         print(f"Running simulation with N={self.N}, q={self.q}, p={self.p}, M={self.M}")
+        for _ in range(self.M): #progressbar(range(self.M), redirect_stdout=True):
+            rng = np.random.default_rng()
+            random_indexes = rng.choice(
+                self.N, 
+                size = self.N , 
+                replace = True)
+            for i in random_indexes:
+                self.voters.try_to_change_opinion_of(i, self.network)
+                #print(self.voters)
 
 
-
+test = VoterModel(10, 2, 0.1, 1, 0.5)
+test.run_simulation()
